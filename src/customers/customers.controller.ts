9be9be1b45fa95995399f0456b7131a795b75297
DiegoY <diego_alex40@hotmail.com>
2025-04-08 10:04:03 -0500

    controladores y metodos HTTP con nest

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Query, Get, ParseIntPipe } from '@nestjs/common';
 
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+
+    //USAR QUERY
+    @Get('query')
+    rutaQuery(@Query() query) {
+        return `El dato query.x ha recibido el valor ${query.x} y el valor de y es: ${query.y}`;
+    }
+
+    @Get('car')
+    carQuery(@Query('count') carCount: number) {
+        return `CarCount: ${carCount}`;
+    }
+
+    @Get('cars')
+    carsQuery(@Query('count', ParseIntPipe) carCount: number) {
+        return `CarCount: ${carCount}`;
+    }
+}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Put, Query, Res } from '@nestjs/common';
+import { response } from 'express';
 import { get } from 'http';
+import { isBooleanObject } from 'util/types';
 
 @Controller('products')
 export class ProductsController {
@@ -22,10 +24,10 @@ export class ProductsController {
 
 //DESESTRUCTURAR PARAMETROS DE URL
 
-    @Get(':id')
-    find(@Param('id') id: number) {
-        return `Pagina del producto ${id}`;
-    }
+    // @Get(':id')
+    // find(@Param('id') id: number) {
+    //     return `Pagina del producto ${id}`;
+    // }
 
 //RECIBIR VARIOS PARAMETROS EN LA URL TIPADOS Y DESAGREGADOS
 
@@ -33,4 +35,62 @@ export class ProductsController {
     findWithSize( @Param('id') id: number, @Param('size') size: string) {
         return `productos con id: ${id} ----- size: ${size}`;
     }
+
+//USO DE POST
+
+    @Post()
+    @HttpCode(HttpStatus.NO_CONTENT)
+    createProduct(@Body() body){
+        return body
+    }
+
+    //@Post()
+    //@HttpCode()
+    //crearteProduct(
+    //    @Body('name') name: string,
+    //    @Body('description') description: string,
+    //) {
+    //    return `Crear producto ${name} con la descripcion: ${description}`;
+    //}
+
+    @Get('ruta-error-404')
+    @HttpCode(HttpStatus.NOT_FOUND)
+    rutaConError404() {
+        return 'Esto es un error 404!! no existe';
+    }
+
+    //DECORADOR RES
+
+    @Get(':id')
+    find(@Res() response, @Param('id') id:number) {
+        if(id<100) {
+            return response.status(HttpStatus.OK).send(`Pagina del producto: ${id}`);
+        } else {
+            return response.status(HttpStatus.NOT_FOUND).send(`Producto inexistente`);
+        }
+    }
+
+    //DECORADOR PUT
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() body) {
+        return `Estas haciendo una operacion de actualizacion del recurso ${id} con ${body.name} 
+        y ${body.description}`;
+    }
+
+    //DECORADOR PATCH
+
+    @Patch(':id')
+    partialUpdate(@Param('id') id: number, @Body() body) {
+        return `Actualización parcial del itam ${id}`;
+    }
+
+    //DECORADOR DELETE
+
+    @Delete(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id') id: number) {
+        return `Hemos borrado el producto ${id}`;
+    }
+
 }
